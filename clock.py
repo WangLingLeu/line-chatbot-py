08cf627828a49bfeb6fededd836790572372b19c
WangLingLeu <145138093+WangLingLeu@users.noreply.github.com>
2023-09-26 09:56:57 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,10 +4,11 @@ import os
 
 # 初始化 Line Bot API
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+user_id = os.environ['TARGET_USER_ID']
 
 # 创建要发送的消息
 message = TextSendMessage(text="快要下班囉")
 
 # 发送消息给指定的用户或群组
-user_id = 'U3e803e6dd2f25dd45a6f3fb763fa5dbf'  # 将 USER_ID_HERE 替换为要发送消息的用户或群组的 ID
+# user_id = 'U3e803e6dd2f25dd45a6f3fb763fa5dbf'  # 将 USER_ID_HERE 替换为要发送消息的用户或群组的 ID
 line_bot_api.push_message(user_id, messages=message)
